218432a827c8752aa27f3b5a8a0f2b479aec772e
Fotios Tragopoulos <fotistragopoulos@icloud.com>
2016-12-22 12:55:11 +0200

    Some changes in the operation process

--- Calculator3D/ViewController.swift
+++ Calculator3D/ViewController.swift
@@ -6,8 +6,9 @@
 import UIKit
 
 class ViewController: UIViewController {
-
-    @IBOutlet weak var outputLbl: UILabel!
+    
+    @IBOutlet weak var landscapeOutputLbl: UILabel!
+    @IBOutlet weak var portraitOutputLbl: UILabel!
     
     enum Operation: String {
         case Divide = "/"
@@ -29,53 +30,235 @@ class ViewController: UIViewController {
 
     override func didReceiveMemoryWarning() {
         super.didReceiveMemoryWarning()
-        // Dispose of any resources that can be recreated.
     }
 
-    @IBAction func numberPressed(sender: UIButton) {
-        runningNumber += "\(sender.tag)"
-        outputLbl.text = runningNumber
+    //Button 0
+    @IBAction func zeroClicked(_ sender: Any) {
+        if landscapeOutputLbl.text != "" {
+            runningNumber += "0"
+            landscapeOutputLbl.text = runningNumber
+            portraitOutputLbl.text = runningNumber
+        }
+    }
+    @IBAction func pzeroClicked(_ sender: Any) {
+        if portraitOutputLbl.text != "" {
+            runningNumber += "0"
+            landscapeOutputLbl.text = runningNumber
+            portraitOutputLbl.text = runningNumber
+        }
     }
     
-    @IBAction func onDividePressed(sender: AnyObject) {
-        processOperation(operation: .Divide)
+    //Button 00
+    @IBAction func doubleZeroClicked(_ sender: Any) {
+        if landscapeOutputLbl.text != "" {
+            runningNumber += "00"
+            landscapeOutputLbl.text = runningNumber
+            portraitOutputLbl.text = runningNumber
+        }
+    }
+    @IBAction func pdoubleZeroClicked(_ sender: Any) {
+        if portraitOutputLbl.text != "" {
+            runningNumber += "00"
+            landscapeOutputLbl.text = runningNumber
+            portraitOutputLbl.text = runningNumber
+        }
+    }
+
+    //Button 000
+    @IBAction func tripleZeroClicked(_ sender: Any) {
+        if landscapeOutputLbl.text != "" {
+            runningNumber += "000"
+            landscapeOutputLbl.text = runningNumber
+            portraitOutputLbl.text = runningNumber
+        }
+    }
+    @IBAction func ptripleZeroClicked(_ sender: Any) {
+        if portraitOutputLbl.text != "" {
+            runningNumber += "000"
+            landscapeOutputLbl.text = runningNumber
+            portraitOutputLbl.text = runningNumber
+        }
     }
     
-    @IBAction func onMultiplyPressed(sender: AnyObject) {
-        processOperation(operation: .Multiply)
+    //Button .
+    @IBAction func commaClicked(_ sender: Any) {
+        if landscapeOutputLbl.text == "" || (landscapeOutputLbl.text?.contains("."))! {
+            
+        } else {
+            runningNumber += "."
+            landscapeOutputLbl.text = runningNumber
+            portraitOutputLbl.text = runningNumber
+        }
+    }
+    @IBAction func pcommaClicked(_ sender: Any) {
+        if landscapeOutputLbl.text == "" || (landscapeOutputLbl.text?.contains("."))! {
+            
+        } else {
+            runningNumber += "."
+            landscapeOutputLbl.text = runningNumber
+            portraitOutputLbl.text = runningNumber
+        }
     }
     
-    @IBAction func onSubtractPressed(sender: AnyObject) {
-        processOperation(operation: .Subtrack)
+    //Button 1
+    @IBAction func oneClicked(_ sender: Any) {
+        runningNumber += "1"
+        landscapeOutputLbl.text = runningNumber
+        portraitOutputLbl.text = runningNumber
+    }
+    @IBAction func poneClicked(_ sender: Any) {
+        runningNumber += "1"
+        landscapeOutputLbl.text = runningNumber
+        portraitOutputLbl.text = runningNumber
     }
     
-    @IBAction func onAddPressed(sender: AnyObject) {
+    //Button 2
+    @IBAction func twoClicked(_ sender: Any) {
+        runningNumber += "2"
+        landscapeOutputLbl.text = runningNumber
+        portraitOutputLbl.text = runningNumber
+    }
+    @IBAction func ptwoClicked(_ sender: Any) {
+        runningNumber += "2"
+        landscapeOutputLbl.text = runningNumber
+        portraitOutputLbl.text = runningNumber
+    }
+    
+    //Button 3
+    @IBAction func threeClicked(_ sender: Any) {
+        runningNumber += "3"
+        landscapeOutputLbl.text = runningNumber
+        portraitOutputLbl.text = runningNumber
+    }
+    @IBAction func pthreeClicked(_ sender: Any) {
+        runningNumber += "3"
+        landscapeOutputLbl.text = runningNumber
+        portraitOutputLbl.text = runningNumber
+    }
+    
+    //Button 4
+    @IBAction func fourClicked(_ sender: Any) {
+        runningNumber += "4"
+        landscapeOutputLbl.text = runningNumber
+        portraitOutputLbl.text = runningNumber
+    }
+    @IBAction func pfourClicked(_ sender: Any) {
+        runningNumber += "4"
+        landscapeOutputLbl.text = runningNumber
+        portraitOutputLbl.text = runningNumber
+    }
+    
+    //Button 5
+    @IBAction func fiveClicked(_ sender: Any) {
+        runningNumber += "5"
+        landscapeOutputLbl.text = runningNumber
+        portraitOutputLbl.text = runningNumber
+    }
+    @IBAction func pfiveClicked(_ sender: Any) {
+        runningNumber += "5"
+        landscapeOutputLbl.text = runningNumber
+        portraitOutputLbl.text = runningNumber
+    }
+    
+    //Button 6
+    @IBAction func sixClicked(_ sender: Any) {
+        runningNumber += "6"
+        landscapeOutputLbl.text = runningNumber
+        portraitOutputLbl.text = runningNumber
+    }
+    @IBAction func psixClicked(_ sender: Any) {
+        runningNumber += "6"
+        landscapeOutputLbl.text = runningNumber
+        portraitOutputLbl.text = runningNumber
+    }
+    
+    //Button 7
+    @IBAction func sevenClicked(_ sender: Any) {
+        runningNumber += "7"
+        landscapeOutputLbl.text = runningNumber
+        portraitOutputLbl.text = runningNumber
+    }
+    @IBAction func psevenClicked(_ sender: Any) {
+        runningNumber += "7"
+        landscapeOutputLbl.text = runningNumber
+        portraitOutputLbl.text = runningNumber
+    }
+   
+    //Buttton 8
+    @IBAction func eightClicked(_ sender: Any) {
+        runningNumber += "8"
+        landscapeOutputLbl.text = runningNumber
+        portraitOutputLbl.text = runningNumber
+    }
+    @IBAction func peightClicked(_ sender: Any) {
+        runningNumber += "8"
+        landscapeOutputLbl.text = runningNumber
+        portraitOutputLbl.text = runningNumber
+    }
+    
+    //Button 9
+    @IBAction func nineClicked(_ sender: Any) {
+        runningNumber += "9"
+        landscapeOutputLbl.text = runningNumber
+        portraitOutputLbl.text = runningNumber
+    }
+    @IBAction func pnineClicked(_ sender: Any) {
+        runningNumber += "9"
+        landscapeOutputLbl.text = runningNumber
+        portraitOutputLbl.text = runningNumber
+    }
+    
+    //Button +
+    @IBAction func addClicked(_ sender: Any) {
+        processOperation(operation: .Add)
+    }
+    @IBAction func paddClicked(_ sender: Any) {
         processOperation(operation: .Add)
     }
     
-    @IBAction func onEqualPressed(sender: AnyObject) {
+    //Button -
+    @IBAction func subtrackClicked(_ sender: Any) {
+    }
+    @IBAction func prsubtrackClicked(_ sender: Any) {
+    }
+    
+    //Button ÷
+    @IBAction func divideClicked(_ sender: Any) {
+    }
+    @IBAction func pdivideClicked(_ sender: Any) {
+    }
+    
+    //Button x
+    @IBAction func multiplyClicked(_ sender: Any) {
+    }
+    @IBAction func pmultiplyClicked(_ sender: Any) {
+    }
+    
+    //Button =
+    @IBAction func resultClicked(_ sender: Any) {
         processOperation(operation: currentOperation)
     }
+    @IBAction func presultClicked(_ sender: Any) {
+        processOperation(operation: currentOperation)
+    }
+ 
+    //Button AC
+    @IBAction func resetClicked(_ sender: Any) {
+        result = ""
+        landscapeOutputLbl.text = result
+        portraitOutputLbl.text = result
+    }
+    @IBAction func presetClicked(_ sender: Any) {
+        result = ""
+        landscapeOutputLbl.text = result
+        portraitOutputLbl.text = result
+    }
     
-    @IBAction func doubleZeroPressed(sender: AnyObject) {
-        runningNumber += "00"
-        outputLbl.text = runningNumber
-    }
-
-    @IBAction func tripleZeroPressed(sender: AnyObject) {
-        runningNumber += "000"
-        outputLbl.text = runningNumber
-    }
     
-    @IBAction func commaPressed(sender: AnyObject) {
-        runningNumber += "."
-        outputLbl.text = runningNumber
-    }
-    
-    @IBAction func acPressed(sender: AnyObject) {
-        runningNumber = ""
-        outputLbl.text = runningNumber
-    }
+//    func outputLbl() {
+//        landscapeOutputLbl.text = result
+//        portraitOutputLbl.text = result
+//    }
     
     func processOperation(operation: Operation) {
         
@@ -97,7 +280,8 @@ class ViewController: UIViewController {
                 }
                 
                 leftValStr = result
-                outputLbl.text = result
+                landscapeOutputLbl.text = result
+                portraitOutputLbl.text = result
             }
             
             currentOperation = operation
@@ -109,5 +293,9 @@ class ViewController: UIViewController {
             currentOperation = operation
         }
     }
+    
+    
+    
+    
 }
 
